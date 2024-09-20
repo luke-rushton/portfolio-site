7f2d5e5873f2d736f2296c7d8f65f0f3fb3f863d
Luke Rushton <luke.rushtonx@gmail.com>
2024-09-20 14:51:41 -0700

    added capability to generate tiles from js obect

---
+++ src/data/testIsland.js
@@ -0,0 +1,89 @@
+const testIsland = [
+    {
+        model: 'hex_coast_A',
+        position: [2, 0, 0],
+        rotation: [0, (4 * -Math.PI) / 3, 0]
+    },
+    {
+        model: 'hex_coast_B',
+        position: [0, 0, 0],
+        rotation: [0, (2 * -Math.PI) / 3, 0]
+    },
+    {
+        model: 'hex_coast_D',
+        position: [1, 0, -Math.sqrt(3)],
+        rotation: [0, (4 * -Math.PI) / 3, 0]
+    },
+    {
+        model: 'hex_coast_E',
+        position: [4, 0, 0],
+        rotation: [0, (2 * -Math.PI) / 3, 0]
+    },
+    {
+        model: 'hex_coast_C',
+        position: [-1, 0, Math.sqrt(3)],
+        rotation: [0, (2 * -Math.PI) / 3, 0]
+    },
+    {
+        model: 'hex_coast_D',
+        position: [3, 0, -Math.sqrt(3)],
+        rotation: [0, (3 * -Math.PI) / 3, 0]
+    },
+    {
+        model: 'hex_coast_C',
+        position: [5, 0, -Math.sqrt(3)],
+        rotation: [0, (4 * -Math.PI) / 3, 0]
+    },
+    {
+        model: 'hex_coast_B',
+        position: [6, 0, 0],
+        rotation: [0, (4 * -Math.PI) / 3, 0]
+    },
+    {
+        model: 'hex_coast_D',
+        position: [7, 0, Math.sqrt(3)],
+        rotation: [0, (0 * -Math.PI) / 3, 0]
+    },
+    {
+        model: 'hex_coast_E',
+        position: [5, 0, Math.sqrt(3)],
+        rotation: [0, (5 * -Math.PI) / 3, 0]
+    },
+    {
+        model: 'hex_coast_D',
+        position: [6, 0, 2 * Math.sqrt(3)],
+        rotation: [0, (0 * -Math.PI) / 3, 0]
+    },
+    {
+        model: 'hex_coast_B',
+        position: [4, 0, 2 * Math.sqrt(3)],
+        rotation: [0, (0 * -Math.PI) / 3, 0]
+    },
+    {
+        model: 'hex_coast_A',
+        position: [2, 0, 2 * Math.sqrt(3)],
+        rotation: [0, (0 * -Math.PI) / 3, 0]
+    },
+    {
+        model: 'hex_coast_D',
+        position: [1, 0, 3 * Math.sqrt(3)],
+        rotation: [0, (1 * -Math.PI) / 3, 0]
+    },
+    {
+        model: 'hex_coast_B',
+        position: [0, 0, 2 * Math.sqrt(3)],
+        rotation: [0, (1 * -Math.PI) / 3, 0]
+    },
+    {
+        model: 'hex_grass',
+        position: [1, 0, 1 * Math.sqrt(3)],
+        rotation: [0, (1 * -Math.PI) / 3, 0]
+    },
+    {
+        model: 'hex_grass',
+        position: [3, 0, 1 * Math.sqrt(3)],
+        rotation: [0, (1 * -Math.PI) / 3, 0]
+    }
+]
+
+export default testIsland;
