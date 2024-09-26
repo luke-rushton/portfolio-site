75b8461eb5ca9313888fc6d85bba22f85fc95bc6
Luke Rushton <luke.rushtonx@gmail.com>
2024-09-25 17:16:47 -0700

    added initial api call to backend

--- src/components/PopupPage.jsx
+++ src/components/PopupPage.jsx
@@ -1,15 +1,33 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { div } from "three/webgpu";
 
 function PopupPage({ active, close, page }) {
+  const restPath = "http://localhost/portfolio/wp-json/wp/v2/pages/8";
+  const [restData, setData] = useState([]);
+  const [isLoaded, setLoadStatus] = useState(false);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch(restPath);
+      if (response.ok) {
+        const data = await response.json();
+        setData(data);
+        setLoadStatus(true);
+      } else {
+        setLoadStatus(false);
+      }
+    };
+    fetchData();
+  }, [restPath]);
+
   return (
     <div className={`popup-mask ${active}`}>
       <article className={`popup`}>
         <button className="close-button" onClick={close}>
           <img src="/x-mark.svg" />
         </button>
-        <h1>{page}</h1>
         {/* ROUTER GOES HERE */}
+        {isLoaded ? <h1>{restData.title.rendered}</h1> : <h1>Loading</h1>}
       </article>
     </div>
   );
