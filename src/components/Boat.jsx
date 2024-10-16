71432c09766467d95013508f8c84a2dc3a0a2204
Luke Rushton <luke.rushtonx@gmail.com>
2024-10-16 15:37:04 -0700

    added boat model, fixed to camera. moved orbitcontrols to boat component

---
+++ src/components/Boat.jsx
@@ -0,0 +1,48 @@
+import React, { useRef, useState } from "react";
+import { useKeyboardControls } from "@react-three/drei";
+import { useFrame } from "@react-three/fiber";
+import * as THREE from "three";
+import { OrbitControls } from "@react-three/drei";
+import { useGLTF } from "@react-three/drei";
+
+function Boat() {
+  const ref = useRef();
+  const boatRef = useRef();
+  const { nodes, materials } = useGLTF("/ship_red_accent.gltf");
+
+  useFrame(() => {
+    //console.log(ref.current.getAzimuthalAngle());
+    //console.log(boatRef.current.position);
+    let x = Math.sin(ref.current.getAzimuthalAngle()) * 14;
+    console.log(x);
+    let z = Math.cos(ref.current.getAzimuthalAngle()) * 14;
+    console.log(z);
+    boatRef.current.position.x = x;
+    boatRef.current.position.z = z;
+    boatRef.current.rotation.y = Math.PI / 2 + ref.current.getAzimuthalAngle();
+  });
+  return (
+    <>
+      <OrbitControls
+        ref={ref}
+        enablePan={false}
+        maxDistance={20}
+        minDistance={17}
+        maxPolarAngle={Math.PI / 2.05}
+        minPolarAngle={Math.PI / 2.2}
+        rotateSpeed={0.25}
+      />
+      <mesh
+        ref={boatRef}
+        position={[14, -0.25, 0]}
+        scale={0.5}
+        castShadow
+        receiveShadow
+        geometry={nodes["ship_red_accent"].geometry}
+        material={materials.hexagons_medieval}
+      ></mesh>
+    </>
+  );
+}
+
+export default Boat;
