84c5903344382583f65a54461c53574cfcfed40d
Luke Rushton <luke.rushtonx@gmail.com>
2024-10-21 11:46:44 -0700

    added animated loading screen to hide threejs load in

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -1,7 +1,26 @@
 import React from "react";
+import { useSpring, animated, useTrail } from "react-spring";
 
 function Loading() {
-  return <h1>I'm Loading!!!!</h1>;
+  const fallingPillars = useTrail(10, {
+    from: { y: "0%" },
+    to: { y: "100%" },
+    delay: 300,
+    config: {
+      mass: 5,
+      tension: 120,
+      friction: 14,
+      clamp: true,
+    },
+  });
+
+  return (
+    <div className="loading">
+      {fallingPillars.map(({ y }, i) => (
+        <animated.div key={i} style={{ y }}></animated.div>
+      ))}
+    </div>
+  );
 }
 
 export default Loading;
