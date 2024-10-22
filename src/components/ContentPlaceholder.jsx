d7c3a54d7a566954d97b69dbc151d08a257e654c
Luke Rushton <luke.rushtonx@gmail.com>
2024-10-22 15:52:42 -0700

    added a proper loading animation to each page

---
+++ src/components/ContentPlaceholder.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function ContentPlaceholder() {
+  return (
+    <div className="placeholder-content">
+      <img src="/loading.svg" alt="Loading symbol" />
+    </div>
+  );
+}
+
+export default ContentPlaceholder;
